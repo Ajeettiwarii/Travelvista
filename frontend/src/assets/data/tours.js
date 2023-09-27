c83ca1598e537a1fd2bf240860613742d70cca97
ajeet <ajeettiwari.ajju21may@gmail.com>
2023-09-27 10:06:50 +0530

    avgrating

--- frontend/src/assets/data/tours.js
+++ frontend/src/assets/data/tours.js
@@ -19,6 +19,10 @@ const tours = [
       {
         name: "jhon doe",
         rating: 4.6,
+      },  
+      {
+        name: "jhon doe",
+        rating: 5,
       },
     ],
     avgRating: 4.5,
