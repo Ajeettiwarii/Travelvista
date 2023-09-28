d604fc9a2ec81b850980cd63ffb503b80f36afe4
ajeet <ajeettiwari.ajju21may@gmail.com>
2023-09-28 22:33:13 +0530

    booking_css

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -4,6 +4,7 @@ import { Form, FormGroup, ListGroup, ListGroupItem, Button } from "reactstrap";
 
 const Booking = ({ tour, avgRating }) => {
   const { price, reviews } = tour;
+  const handleChange = (e) => {};
   return (
     <div className="booking">
       <div className="booking__top d-flex align-items-center justify-content-between">
@@ -14,25 +15,45 @@ const Booking = ({ tour, avgRating }) => {
           <i class="ri-star-s-fill"></i>
           {avgRating === 0 ? null : avgRating}({reviews?.length})
         </span>
-      </div>   
+      </div>
       <div className="booking__form">
-        <h5>Information</h5>  
+        <h5>Information</h5>
         <Form className="booking__info-form">
-            <FormGroup>
-                  <input type="text" placeholder="Full Name" id="fullName"
-                   required onChange={handleChange} />
-            </FormGroup>
-            <FormGroup>
-                  <input type="number" placeholder="Full Name" id="fullName"
-                   required onChange={handleChange} />
-            </FormGroup>
-            <FormGroup>
-                  <input type="text" placeholder="Full Name" id="fullName"
-                   required onChange={handleChange} />
-            </FormGroup>
-
+          <FormGroup>
+            <input
+              type="text"
+              placeholder="Full Name"
+              id="fullName"
+              required
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <input
+              type="number"
+              placeholder="Phone"
+              id="Phone"
+              required
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup className="d-flex align-items-center gap-3">
+            <input
+              type="date"
+              placeholder=""
+              id="bookAt"
+              required
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              placeholder="Guest"
+              id="guestSize"
+              required
+              onChange={handleChange}
+            />
+          </FormGroup>
         </Form>
-
       </div>
     </div>
   );
