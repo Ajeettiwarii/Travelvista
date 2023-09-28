58f37795644cd1f97c01a4ef347c313b47b588e8
ajeet <ajeettiwari.ajju21may@gmail.com>
2023-09-28 09:49:02 +0530

    scroll

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useRef,useEffect}from "react";
 import { Container, Row, Button } from "reactstrap";
  import { NavLink} from 'react-router-dom';
 import logo1 from "../../assets/images/logo1.jpeg";
@@ -19,10 +19,25 @@ const nav__links = [
     display: "Tours",
   },
 ];
+  
+const Header = () => {     
+  const headerRef=useRef(null)  
+  const stickyHeaderFunc=()=>{
+    window.addEventListener('scroll',()=>{
+      if(document.body.scrollTop>80 || document.documentElement.scrollTop>80){
+        headerRef.current.classList.add('sticky__header')
+      }else{  
+        headerRef.current.classList.remove('sticky__header')
 
-const Header = () => {
+      }
+    })
+  }   
+  useEffect(()=>{
+    stickyHeaderFunc()
+    return window.removeEventListener('scroll',stickyHeaderFunc)
+  })
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <Row>
           <div
